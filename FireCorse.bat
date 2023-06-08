adc607bbd006d253a6ee0d3a96d665d9ba4890e7
Jeg27 <114416002+Jeg27@users.noreply.github.com>
2023-06-08 14:08:49 -0400

    Add files via upload

---
+++ FireCorse.bat
@@ -0,0 +1,3 @@
+C:/eclipse/plugins/CD++Builder_1.1.0/internal/simuOrig.exe  -mFireCorseMA.ma  -lFireCorse.log -t00:00:00:150 
+rem 
+pause
---
+++ FireCorseDRW.bat
@@ -0,0 +1,2 @@
+drawlog  -mFireCorseMA.ma -cForestFire -lFireCorse.log -w7 -p1 -0 > FireCorse.drw
+pause
